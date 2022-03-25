6427f622d8ac40b9eea63c7716c4de5c65ac1b4c
Vladislav Zverev <vladspbru@gmail.com>
2022-03-25 10:19:56 +0300

    +antlibstring

---
+++ qt/antlibstring.h
@@ -0,0 +1,42 @@
+#pragma once
+
+
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include <iomanip>
+
+
+class AntLibString : public std::string
+{
+public:
+  AntLibString():std::string() {}
+
+  int Pos(const std::string& subStr) const { return this->find_first_of(subStr); }
+
+  int Length() const { return size(); }
+
+  int ToInt() const {
+    try {
+      return std::stoi(*this);
+    } catch (const std::invalid_argument& e) {
+      std::cerr<<__FUNCTION__<<e.what()<<std::endl;
+    }
+    return 0;
+    //return std::atoi( this->c_str() );
+  }
+
+  int ToIntDef(int defaultValue) const {
+    try {
+      return std::stoi(*this);
+    } catch (...) {
+    }
+    return defaultValue;
+  }
+
+
+};
+
+
+
+
--- qt/vcl.h
+++ qt/vcl.h
@@ -1,20 +1,31 @@
 #ifndef VCL_H
 #define VCL_H
 
-#include <QString>
-class String :public QString
-{
-public:
-    int Pos(const String& subStr) const { return indexOf(subStr);}
-    int Length() const {return size();}
-    int ToInt() const {return toInt(); }
-    int ToIntDef(int defaultValue) const {
-        bool ok=false;
-        int r=toInt(&ok);
-        if (!ok) r=defaultValue;
-        return r;
-    }
 
-};
+#include "antlibstring.h"
+typedef AntLibString String;
+
+
+
+
+//#include <QString>
+//class String :public QString
+//{
+//public:
+//  int Pos(const String& subStr) const { return indexOf(subStr);}
+//  int Length() const {return size();}
+//  int ToInt() const {return toInt(); }
+//  int ToIntDef(int defaultValue) const {
+//    bool ok=false;
+//    int r=toInt(&ok);
+//    if (!ok) r=defaultValue;
+//    return r;
+//  }
+
+//};
+
+
+
+
 
 #endif // VCL_H
