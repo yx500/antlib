18f486c9234613fecd51d775100b9c4e691d3d25
Vladislav Zverev <vladspbru@gmail.com>
2022-03-28 17:00:50 +0300

    .

--- qt/vcl.h
+++ qt/vcl.h
@@ -1,33 +1,7 @@
-#ifndef VCL_H
-#define VCL_H
-
+#pragma once
 
 
 #include "antlibstring.h"
 typedef AntLibString String;
 #include "antlibstringlist.h"
 
-
-
-
-
-//#include <QString>
-//class String :public QString
-//{
-//
-//  int Pos(const String& subStr) const { return indexOf(subStr);}
-//  int Length() const {return size();}
-//  int ToInt() const {return toInt(); }
-//  int ToIntDef(int defaultValue) const {
-//    bool ok=false;
-//    int r=toInt(&ok);
-//    if (!ok) r=defaultValue;
-//    return r;
-//  }public:
-
-//};
-
-
-
-
-#endif // VCL_H
