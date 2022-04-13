c89820b6c1b3a6831f63456e0422dd347f39f3f7
Vladislav Zverev <vladspbru@gmail.com>
2022-04-13 17:21:26 +0300

    .

--- qt/antlibinifile.cpp
+++ qt/antlibinifile.cpp
@@ -2,7 +2,7 @@
 
 AIniFile::AIniFile(const String& FileName) : QSettings(FileName.c_str(), QSettings::IniFormat)
 {
-  this->setIniCodec("Windows-1251");
+//todo  this->setIniCodec("Windows-1251");
 }
 
 bool AIniFile::SectionExists(const String& c)
